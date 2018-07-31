0ee25713ce02a7ca907085cc09a40c26512ee127
WHG555 <whghcyx@outlook.com>
2018-07-31 17:02:03 +0800

    调整了代码结构

--- main.c
+++ main.c
@@ -1,32 +1,31 @@
 /*************************************************************************
  * 文件名称：main.c
  * 作    者：whg
- * 创建时间：2018年07月30日 星期一 20时49分30秒
+ * 创建时间：2018年07月30日 星期一 22时31分40秒
  * 模块名称：
  * 主要函数：
  * 版    本：
  * 修改记录：
  ************************************************************************/
 
-#include <stdio.h>
-#include <stdlib.h>
+#include "mpc.h"
+
 #ifdef _WIN32
-#include <string.h>
+
 static char buffer[2048];
 
-char* readline(char* prompt)
+char* readline(char* prompt) 
 {
 	fputs(prompt, stdout);
 	fgets(buffer, 2048, stdin);
-
-	char *cpy = malloc(strlen(buffer)+1);
-
+	char* cpy = malloc(strlen(buffer)+1);
 	strcpy(cpy, buffer);
 	cpy[strlen(cpy)-1] = '\0';
-
 	return cpy;
 }
+
 void add_history(char* unused) {}
+
 #else
 #include <readline/readline.h>
 #include <readline/history.h>
@@ -34,19 +33,41 @@ void add_history(char* unused) {}
 
 int main(int argc, char** argv)
 {
-	puts("Smile Version 0.1");
+	mpc_parser_t* Number   = mpc_new("number");
+	mpc_parser_t* Operator = mpc_new("operator");
+	mpc_parser_t* Expr     = mpc_new("expr");
+	mpc_parser_t* Smile    = mpc_new("smile");
+
+	mpca_lang(MPCA_LANG_DEFAULT,
+    "                                                     \
+      number   : /-?[0-9]+/ ;                             \
+      operator : '+' | '-' | '*' | '/' ;                  \
+      expr     : <number> | '(' <operator> <expr>+ ')' ;  \
+      smile    : /^/ <operator> <expr>+ /$/ ;             \
+    ",
+    Number, Operator, Expr, Smile);
+
+	puts("Smile Version 0.2");
 	puts("Press Ctrl+c to Exit\n");
 
-	while(1){
+	while(1) {
+		
 		char* input = readline("Smile> ");
-
 		add_history(input);
 
-		printf("No you're a %s\n", input);
+		mpc_result_t r;
+		if(mpc_parse("<stdin>", input, Smile, &r)) {
+			mpc_ast_print(r.output);
+			mpc_ast_delete(r.output);
+		}else{
+			mpc_err_print(r.error);
+			mpc_err_delete(r.error);
+		}
 
 		free(input);
 	}
 
+	mpc_cleanup(4, Number, Operator, Expr, Smile);
+
 	return 0;
 }
-
--- makefile
+++ makefile
@@ -1,11 +1,14 @@
 objects=main.o
 CC=gcc
 CFG=-std=c99 -Wall
-LIB=-lreadline
+LIB=-lm -lreadline
 
-nn: main.o
+nn: main.o mpc.o
 	gcc $(CFG) -o $@ $^    $(LIB)
 main.o:main.c
 	gcc $(CFG) -o $@ -c $^ $(LIB)
+mpc.o:mpc.c
+	gcc $(CFG) -o $@ -c $^ $(LIB)
+
 clean:
 	rm nn $(objects)
