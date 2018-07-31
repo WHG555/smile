fcde53901c2988a064241efe8250fd6b6e877546
WHG555 <whghcyx@outlook.com>
2018-07-31 17:21:12 +0800

    Smile v0.3 code

--- main.c
+++ main.c
@@ -1,73 +1,102 @@
-/*************************************************************************
- * 文件名称：main.c
- * 作    者：whg
- * 创建时间：2018年07月30日 星期一 22时31分40秒
- * 模块名称：
- * 主要函数：
- * 版    本：
- * 修改记录：
- ************************************************************************/
-
 #include "mpc.h"
 
 #ifdef _WIN32
 
 static char buffer[2048];
 
-char* readline(char* prompt) 
+char *readline(char *prompt)
 {
 	fputs(prompt, stdout);
 	fgets(buffer, 2048, stdin);
-	char* cpy = malloc(strlen(buffer)+1);
-	strcpy(cpy, buffer);
-	cpy[strlen(cpy)-1] = '\0';
+	
+	char *cpy = malloc(strlen(buffer) + 1);
+	
+	strcpy(cpy, buufer);
+	cpy[strlen(cpy) - 1] = '\0';
+	
 	return cpy;
 }
 
-void add_history(char* unused) {}
+void add_history(char *unused){ };
 
 #else
+
 #include <readline/readline.h>
 #include <readline/history.h>
+
 #endif
 
-int main(int argc, char** argv)
+long eval_op(long x, char *op, long y)
 {
-	mpc_parser_t* Number   = mpc_new("number");
-	mpc_parser_t* Operator = mpc_new("operator");
-	mpc_parser_t* Expr     = mpc_new("expr");
-	mpc_parser_t* Smile    = mpc_new("smile");
+	if(strcmp(op, "+") == 0)
+		return x + y;
+	if(strcmp(op, "-") == 0)
+		return x - y;
+	if(strcmp(op, "*") == 0)
+		return x * y;
+	if(strcmp(op, "/") == 0)
+		return x / y;
+	return 0;
+}
 
-	mpca_lang(MPCA_LANG_DEFAULT,
-    "                                                     \
-      number   : /-?[0-9]+/ ;                             \
-      operator : '+' | '-' | '*' | '/' ;                  \
-      expr     : <number> | '(' <operator> <expr>+ ')' ;  \
-      smile    : /^/ <operator> <expr>+ /$/ ;             \
-    ",
-    Number, Operator, Expr, Smile);
-
-	puts("Smile Version 0.2");
-	puts("Press Ctrl+c to Exit\n");
-
-	while(1) {
+long eval(mpc_ast_t *t)
+{
+	if(strstr(t->tag, "number"))
+		return atoi(t->contents);
 		
-		char* input = readline("Smile> ");
-		add_history(input);
+	char *op =  t->children[1]->contents;
+	long x = eval(t->children[2]);
+	int i = 3;
+	while(strstr(t->children[i]->tag, "expr"))
+	{
+		x = eval_op(x, op, eval(t->children[i]));
+		i++;
+	}
+	
+	return x;
+}
 
+int main(int argc, char **argv)
+{
+	mpc_parser_t *Number = mpc_new("number");
+	mpc_parser_t *Operator = mpc_new("operator");
+	mpc_parser_t *Expr = mpc_new("expr");
+	mpc_parser_t *Smile = mpc_new("smile");
+	
+	mpca_lang(MPCA_LANG_DEFAULT,
+	"											\
+	number		: /-?[0-9]+/ ;					\
+	operator	: '+' | '-' | '*' | '/';		\
+	expr		: <number> | '(' <operator> <expr>+ ')';	\
+	smile		: /^/ <operator> <expr>+ /$/;	\
+	",
+	Number, Operator, Expr, Smile);
+	
+	puts("Smile Version 0.3");
+	puts("Press Ctrl+c to exit\n");
+	
+	while(1)
+	{
+		char *input = readline("Smile> ");
+		add_history(input);
+		
 		mpc_result_t r;
-		if(mpc_parse("<stdin>", input, Smile, &r)) {
-			mpc_ast_print(r.output);
+		if(mpc_parse("<stdin>", input, Smile, &r))
+		{
+			long result = eval(r.output);
+			printf("%li\n", result);
 			mpc_ast_delete(r.output);
-		}else{
+		}
+		else
+		{
 			mpc_err_print(r.error);
 			mpc_err_delete(r.error);
 		}
-
+		
 		free(input);
 	}
-
+	
 	mpc_cleanup(4, Number, Operator, Expr, Smile);
-
+	
 	return 0;
 }
